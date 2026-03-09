f387d86e84f26c6c8dd6ed5cb32e6ac9d20ad3f9
oksanakolomenko <oksana.kolomenko@student.htw-berlin.de>
2026-03-09 09:28:54 +0200

    last todo

--- src/hyperparam_builder.py
+++ src/hyperparam_builder.py
@@ -1,31 +1,12 @@
 from abc import ABC, abstractmethod
 
+from config.config_manager import ConfigManager
 from src.exp_context import ExpContext
 
 
 def prefix_grid(grid: dict[str, list], prefix: str) -> dict[str, list]:
     return {f"{prefix}{k}": v for k, v in grid.items()}
 
-LR_GRID = {
-    "classifier__C": [2, 10, 50],
-}
-
-GBDT_GRID = {
-    "classifier__min_samples_leaf": [5, 10, 15, 20],
-}
-
-TEXT_GRID = {
-    "embedding_aggregator__method": [
-        "embedding_cls",
-        "embedding_mean_with_cls_and_sep",
-        "embedding_mean_without_cls_and_sep",
-    ]
-}
-
-RTE_GRID = {
-    "embedding__n_estimators": [10, 100],
-    "embedding__max_depth": [2, 5],
-}
 
 class ParamGridStrategy(ABC):
     @abstractmethod
@@ -36,14 +17,17 @@ class ParamGridStrategy(ABC):
 class BasicGrid(ParamGridStrategy):
     def build(self, ctx:ExpContext):
         if ctx.flags.is_lr:
+            LR_GRID = ctx.cfg.hyperparams_cfg['lr']
             return LR_GRID
         elif ctx.flags.is_gbdt:
+            GBDT_GRID = ctx.cfg.hyperparams_cfg['gbdt']
             return GBDT_GRID
         raise NotImplementedError
 
 class RTEGrid(ParamGridStrategy):
     def build(self, ctx):
-        base = LR_GRID if ctx.flags.is_lr else GBDT_GRID
+        base = ctx.cfg.hyperparams_cfg['lr'] if ctx.flags.is_lr else ctx.cfg.hyperparams_cfg['gbdt']
+        RTE_GRID = ctx.cfg.hyperparams_cfg['rte']
         return {
             **base,
             **RTE_GRID,
@@ -51,7 +35,8 @@ class RTEGrid(ParamGridStrategy):
 
 class TextGrid(ParamGridStrategy):
     def build(self, ctx):
-        base = LR_GRID if ctx.flags.is_lr else GBDT_GRID
+        base = ctx.cfg.hyperparams_cfg['lr'] if ctx.flags.is_lr else ctx.cfg.hyperparams_cfg['gbdt']
+        TEXT_GRID = ctx.cfg.hyperparams_cfg['text_emb']
         return {
             **base,
             **TEXT_GRID,
@@ -59,7 +44,8 @@ class TextGrid(ParamGridStrategy):
 
 class ConcatTextGrid(ParamGridStrategy):
     def build(self, ctx):
-        base = LR_GRID if ctx.flags.is_lr else GBDT_GRID
+        base = ctx.cfg.hyperparams_cfg['lr'] if ctx.flags.is_lr else ctx.cfg.hyperparams_cfg['gbdt']
+        TEXT_GRID = ctx.cfg.hyperparams_cfg['text_emb']
         return {
             **base,
             **prefix_grid(TEXT_GRID, "transformer__text__"),
@@ -68,7 +54,8 @@ class ConcatTextGrid(ParamGridStrategy):
 
 class ConcatRTEGrid(ParamGridStrategy):
     def build(self, ctx):
-        base = LR_GRID if ctx.flags.is_lr else GBDT_GRID
+        base = ctx.cfg.hyperparams_cfg['lr'] if ctx.flags.is_lr else ctx.cfg.hyperparams_cfg['gbdt']
+        RTE_GRID = ctx.cfg.hyperparams_cfg['rte']
         return {
             **base,
             **prefix_grid(RTE_GRID, "feature_combiner__embeddings__"),
